06ebf17787b3e0b05ec612a3ba859eda80232032
Feng Zhou <zhfe99@gmail.com>
2018-09-29 10:26:38 +0800

    Init

---
+++ lib/gph/matchAsg.m
@@ -0,0 +1,29 @@
+function acc = matchAsg(X, asgT)
+% Find the match between two assignments and compute the accuracy.
+%
+% Input
+%   X       -  original assignment matrix, n1 x n2
+%   asgT    -  ground-truth assignment (can be [])
+%
+% Output
+%   acc     -  accuracy of the matching
+%
+% History
+%   create  -  Feng Zhou (zhfe99@gmail.com), 01-22-2009
+%   modify  -  Feng Zhou (zhfe99@gmail.com), 02-14-2012
+
+% ground-truth
+XT = ps(asgT, 'X', []);
+if isempty(XT)
+    acc = 0;
+    return;
+end
+
+% non-zero correspondence
+idx = find(XT);
+
+% #correct correspondences found
+co = length(find(XT(idx) == X(idx)));
+
+% percentage
+acc = co / length(idx);
